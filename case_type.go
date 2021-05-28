8a167fdbfd4ce152632d1d23d49217369b17a4af
Brad Babb <brad_babb@cable.comcast.com>
2021-05-28 17:54:58 -0500

    update to use enumer

--- case_type.go
+++ case_type.go
@@ -1,3 +1,3 @@
 package caser
 
-//go:generate enumeration -package=caser -name=CaseType -items=camel,snake,snakeUpper,kebab,kebabUpper,pascal,phrase,unknown
+//go:generate enumer -package=caser -name=CaseType -items=camel,snake,snakeUpper,kebab,kebabUpper,pascal,phrase,unknown
